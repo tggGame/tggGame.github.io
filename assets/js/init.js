21428027f0abc27d687a65a6eafe6efc51d48fdf
tggSearch <huangdh4@outlook.com>
2024-07-14 09:41:30 +0800

    init

--- assets/js/init.js
+++ assets/js/init.js
@@ -8,42 +8,42 @@
 
 (function($) {
 
-	// skel.init({
-	// 	reset: 'full',
-	// 	breakpoints: {
-	// 		global:		{ range: '*', href: '{{ site.baseurl }}/assets/css/style.css', containers: 1400, grid: { gutters: 50 } },
-	// 		wide:		{ range: '-1680', href: '{{ site.baseurl }}/assets/css/style-wide.css', containers: 1200, grid: { gutters: 40 } },
-	// 		normal:		{ range: '-1280', href: '{{ site.baseurl }}/assets/css/style-normal.css', containers: 960, grid: { gutters: 30 }, viewport: { scalable: false } },
-	// 		narrow:		{ range: '-980', href: '{{ site.baseurl }}/assets/css/style-narrow.css', containers: '95%', grid: { gutters: 20 } },
-	// 		narrower:	{ range: '-840', href: '{{ site.baseurl }}/assets/css/style-narrower.css', grid: { collapse: 1 } },
-	// 		mobile:		{ range: '-736', href: '{{ site.baseurl }}/assets/css/style-mobile.css', containers: '90%', grid: { gutters: 15 } },
-	// 		mobilep:	{ range: '-480', href: '{{ site.baseurl }}/assets/css/style-mobilep.css', grid: { collapse: 2 }, containers: '100%' }
-	// 	},
-	// 	plugins: {
-	// 		layers: {
-	// 			navPanel: {
-	// 				animation: 'revealX',
-	// 				breakpoints: 'narrower',
-	// 				clickToHide: true,
-	// 				height: '100%',
-	// 				hidden: true,
-	// 				html: '<div data-action="navList" data-args="nav"></div>',
-	// 				orientation: 'vertical',
-	// 				position: 'top-left',
-	// 				side: 'left',
-	// 				width: 275
-	// 			},
-	// 			titleBar: {
-	// 				breakpoints: 'narrower',
-	// 				height: 44,
-	// 				html: '<span class="toggle" data-action="toggleLayer" data-args="navPanel"></span><span class="title" data-action="copyHTML" data-args="logo"></span>',
-	// 				position: 'top-left',
-	// 				side: 'top',
-	// 				width: '100%'
-	// 			}
-	// 		}
-	// 	}
-	// });
+	skel.init({
+		reset: 'full',
+		breakpoints: {
+			global:		{ range: '*', href: '{{ site.baseurl }}/assets/css/style.css', containers: 1400, grid: { gutters: 50 } },
+			wide:		{ range: '-1680', href: '{{ site.baseurl }}/assets/css/style-wide.css', containers: 1200, grid: { gutters: 40 } },
+			normal:		{ range: '-1280', href: '{{ site.baseurl }}/assets/css/style-normal.css', containers: 960, grid: { gutters: 30 }, viewport: { scalable: false } },
+			narrow:		{ range: '-980', href: '{{ site.baseurl }}/assets/css/style-narrow.css', containers: '95%', grid: { gutters: 20 } },
+			narrower:	{ range: '-840', href: '{{ site.baseurl }}/assets/css/style-narrower.css', grid: { collapse: 1 } },
+			mobile:		{ range: '-736', href: '{{ site.baseurl }}/assets/css/style-mobile.css', containers: '90%', grid: { gutters: 15 } },
+			mobilep:	{ range: '-480', href: '{{ site.baseurl }}/assets/css/style-mobilep.css', grid: { collapse: 2 }, containers: '100%' }
+		},
+		plugins: {
+			layers: {
+				navPanel: {
+					animation: 'revealX',
+					breakpoints: 'narrower',
+					clickToHide: true,
+					height: '100%',
+					hidden: true,
+					html: '<div data-action="navList" data-args="nav"></div>',
+					orientation: 'vertical',
+					position: 'top-left',
+					side: 'left',
+					width: 275
+				},
+				titleBar: {
+					breakpoints: 'narrower',
+					height: 44,
+					html: '<span class="toggle" data-action="toggleLayer" data-args="navPanel"></span><span class="title" data-action="copyHTML" data-args="logo"></span>',
+					position: 'top-left',
+					side: 'top',
+					width: '100%'
+				}
+			}
+		}
+	});
 
 	$(function() {
 
